5fb9f0bbfc3e7b06382e1917502579fe4fb952dc
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-07-25 10:14:10 +0200

    Add routing and blog pages

---
+++ app/blog/[id].js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export default function Post({ post }) {
+  return (
+    <>
+      <main>
+        <Link href="/" alt="">
+          <a>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </a>
+        </Link>
+      </main>
+    </>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  ).then((r) => r.json());
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(
+    (r) => r.json()
+  );
+  return {
+    paths: posts.map((post) => ({
+      params: { id: post.id.toString() },
+    })),
+    fallback: false,
+  };
+}
--- app/page.js
+++ app/page.js
@@ -1,29 +1,45 @@
 "use client";
 import Head from "next/head";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 
-export default function Home({posts}) {
-const [count, setCount] = useState(0);
+export default function Home() {
+  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
 
-useEffect(() => {
-  const timer = setInterval(() => setCount(n => n+1), 1000)
-  return () => {
-    clearInterval(timer)
-  }
-}, [])
+  useEffect(() => {
+    const timer = setInterval(() => setCount((n) => n + 1), 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    const data = fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((r) => r.json())
+      .then(setPosts);
+  }, []);
+
   return (
     <>
       <Head>
         <title>My Great blog</title>
       </Head>
       <h1>Count : {count}</h1>
-      {/* <ul>
+      <ul>
         {posts.map((post) => (
           <li>
-            <h3>{post.title}</h3>
+            <Link href={`/blog/${post.id}`} alt="">
+              <a>
+                <h3>
+                  {post.id} - 
+                  {post.title}
+                </h3>
+              </a>
+            </Link>
           </li>
         ))}
-      </ul> */}
+      </ul>
     </>
   );
 }
